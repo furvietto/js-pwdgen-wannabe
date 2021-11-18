36dcfa76610215a2b4071d4698496cc3b7391eff
furvietto <fulvio-94@hotmail.it>
2021-11-18 14:26:16 +0100

    adding some trying

--- js/script.js
+++ js/script.js
@@ -7,18 +7,22 @@
 const name = prompt("come ti chiami");
 console.log(name);
 
-const surname = prompt("bravo,ora il tuo cognome");
+let cognome = "bravo, ora il tuo cognome";
+let allerta = " sono nell'alert"
+
+const surname = prompt(cognome);
 console.log(surname);
 
 let color = prompt ("ma sei un genio, adesso il colore preferito");
 console.log(color);
 
 let numero = 10;
-numero = numero + 11;
+numero += 11;
 
 
 let somma = `allora ti chiami ${name} ${surname}, il tuo colore preferito è ${color} e numero a caso ${numero}`;
 
-document.getElementById("js").innerHTML = somma;
-document.writeln(somma);
+document.getElementById("js").innerHTML = somma + " sono nel getelement";
+document.writeln(somma + " sono nel writeln");
+alert(somma + allerta);
 console.log(somma);
