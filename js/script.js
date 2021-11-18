8f81075c6f548427ad41a5d2aa87cb5752b5d136
furvietto <fulvio-94@hotmail.it>
2021-11-18 14:06:44 +0100

    work done

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@
 //3 poi chiedi il suo colore preferito
 //4 Infine scrivi sulla pagina nomecognomecolorepreferito21
 
+const name = prompt("come ti chiami");
+console.log(name);
+
+const surname = prompt("bravo,ora il tuo cognome");
+console.log(surname);
+
+let color = prompt ("ma sei un genio, adesso il colore preferito");
+console.log(color);
+
+let numero = 10;
+numero = numero + 11;
+
+
+let somma = `allora ti chiami ${name} ${surname}, il tuo colore preferito è ${color} e numero a caso ${numero}`;
+
+document.getElementById("js").innerHTML = somma;
+document.writeln(somma);
+console.log(somma);
